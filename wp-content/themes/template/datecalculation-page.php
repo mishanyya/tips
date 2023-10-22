c275a66fffa112ca74ed64c1cab3b20c0849b7f0
mishanyya <mishanyakashin@gmail.com>
2023-10-22 18:37:28 +0300

    22 октября 23 все расчеты работают на большом экране

--- wp-content/themes/template/datecalculation-page.php
+++ wp-content/themes/template/datecalculation-page.php
@@ -117,12 +117,11 @@ get_header();
         </script>
 
           <script>
-        //  function funcajax(){
-            jQuery( function( $ ){ // есть разные варианты этой строчки, но такая мне нравится больше всего, т.к. всегда работает
+            jQuery( function( $ ){ // добавить даты
   	$( '#idajax' ).click( function(){ // при клике на элемент с id="idajax"
   		//alert( 'JQuery у меня вроде работает!!!' ); // выводим сообщение просто для проверки работает ли jquery
-      var x=document.getElementsByClassName('day1')[0].value;
-      var y=document.getElementsByClassName('day2')[0].value;
+      let x=document.getElementsByClassName('day1')[0].value;
+      let y=document.getElementsByClassName('day2')[0].value;
       //сравнение дат, если идет большая, затем меньшая - они меняются местами
       let resdate1=x.split(".");
       let resdate2=y.split(".");
@@ -142,19 +141,18 @@ get_header();
         y=xy;
         }
 
-     var adress="<?php
+     let adress="<?php
      echo get_template_directory_uri().'/php/calculatedifdate.php';//URL папки шаблона WordPress, например http://test/wp-content/themes/template
        ?>";
 
-  //  alert(adress);
 //ajax запрос
     $.ajax({
-	url: adress, //'/index.php',         /* Куда пойдет запрос */
+	url: adress,               /* Куда пойдет запрос */
 	method: 'get',             /* Метод передачи (post или get) */
-	dataType: 'html',          /* Тип данных в ответе (xml, json, script, html). */
-	//data: {param1: x},     /* Параметры передаваемые в запросе. */
+	dataType: 'html',          /* Тип данных в ответе */
+	//data: {param1: x},       /* Параметры передаваемые в запросе. */
 	data: 'param1='+x+'&param2='+y,     /* Параметры передаваемые в запросе. */
-  //req.open('GET', 'example.php?var='+varjs, true);
+
 	success: function(data){   /* функция которая будет выполнена после успешного запроса.  */
     data = data.split('-', 3); //разбить строку на подстроки по
 		document.getElementsByClassName('dday')[0].innerHTML=data[2];            /* В переменной data содержится ответ от ajax c сервера/файла php. */
@@ -168,20 +166,13 @@ $(".list-group").append("<li class='list-group-item'>"+x+" - "+y+"</li>" );
 });
   	});
   });
-      //    }
-////////////////
 
-//  function funcajax(){
-    jQuery( function( $ ){ // есть разные варианты этой строчки, но такая мне нравится больше всего, т.к. всегда работает
-$( '#idclear' ).click( function(){ // при клике на элемент с id="idajax"
-//alert( 'JQuery у меня вроде работает!!!' ); // выводим сообщение просто для проверки работает ли jquery
+
+  jQuery( function( $ ){ // сбросить
+$( '#idclear' ).click( function(){ // при клике на элемент с id="idclear"
 
 document.getElementsByClassName('day1')[0].value='';
 document.getElementsByClassName('day2')[0].value='';
-rez=0;
-dd=0;
-mm=0;
-yy=0;
 document.getElementsByClassName('daysum')[0].innerHTML='';            /* В переменной data содержится ответ от ajax c сервера/файла php. */
 document.getElementsByClassName('monthsum')[0].innerHTML='';            /* В переменной data содержится ответ от ajax c сервера/файла php. */
 document.getElementsByClassName('yearsum')[0].innerHTML='';
@@ -193,154 +184,25 @@ $('.list-group').empty();//удалить все элементы внутри �
 });
 });
 
-//  function sort(){
-    jQuery(function($){ // есть разные варианты этой строчки, но такая мне нравится больше всего, т.к. всегда работает
-$( '#idsort' ).click( function(){ // при клике на элемент с id="idajax"
-//alert( 'JQuery у меня вроде работает!!!' ); // выводим сообщение просто для проверки работает ли jquery
-
-var ar=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
-var ll=ar.length;  //кол-во элементов с классом list-group-item
-
-var data1;
-var data2;
-let d1,d11;
-let d2,d22;
-let el,el0;
-let el1;
-let el2=1;
-let dd;
-let answer=0;//наличие совпадений
-let arr = []; //массив для всех дат
-
-for(el0=0;el0<ll;el0++){  //0 цикл
-for(el1=0;el1<ll;el1++,el2++){  //1 цикл для первой даты
-  data1 = ar[el1].innerHTML.split('.', 3); //разбить строку на подстроки по знаку '.'
-  data2=data1[2].split('-',1);                  //разбить строку на подстроки по знаку '.-'
-  d1=data1[0]+'.'+data1[1]+'.'+data2[0];
-  d11=data2[0]+'.'+data1[1]+'.'+data1[0];
-
-for(el=el2;el<ll;el++){  //2 цикл для второй даты
-  data1 = ar[el].innerHTML.split('.', 3); //разбить строку на подстроки по знаку '.'
-  data2=data1[2].split('-',1);                  //разбить строку на подстроки по знаку '.-'
-  d2=data1[0]+'.'+data1[1]+'.'+data2[0];
-  d22=data2[0]+'.'+data1[1]+'.'+data1[0];
-
-  //alert(d1+'+'+d2);
-  //alert(d11+'++'+d22);
-  if(d11>d22){
-//    alert(d1+'>'+d2);
-//ar[el1].after('after'); // вставить строку "after" ПОСЛЕ выбранного html элемента
-//ar[el1].before('before'); // вставить строку "before" ПЕРЕД выбранным html элементом
-ar[el1].before(ar[el]); // вставить строку "before" ПЕРЕД выбранным html элементом
-}
-}    //2 цикл
-}    //1 цикл
-}   //0 цикл
-
-
-
-
-for(el=0;el<(ll-1);el++){  //цикл для поиска совпадений
-  //поиск совпадений дат
-if(ar[el].innerHTML==ar[el+1].innerHTML){
-  answer=1;
-  // добавление элементу класс
-ar[el].classList.add("doubleremove");
-ar[el].classList.add("text-danger");
-}
-//поиск совмащенных дат
-}//конец цикла
-
-if(answer==1){
-  document.getElementsByClassName('double')[0].innerHTML="Найдены совпадения!";
-  // добавление элементу класс
-document.getElementsByClassName('double')[0].classList.add("text-danger");
-document.getElementById('iddoubleclear').classList.remove("invisible");
-document.getElementById('iddoubleclear').classList.add("visible");
-}
-
-
-
-//найти совмещенные даты
-
-let ar1=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
-let ll1=ar1.length;  //кол-во элементов с классом list-group-item
-let da1;
-let da2;
-let da11;
-let da21;
-let date1a;
-let date2a;
-var i;
-for(i=0;i<ll1;i++){
-  da1=ar1[i].innerHTML.split("-");
-  da2=ar1[i+1].innerHTML.split("-");
-
-let da11=da1[1].split(".");
-let da21=da2[0].split(".");
-
-  date1a = new Date(da11[2],da11[1],da11[0]);
-  date2a = new Date(da21[2],da21[1],da21[0]);
-  if(date1a>date2a){
-    ar1[i].classList.add("bg-secondary");
-    ar1[i+1].classList.add("bg-secondary");
-    document.getElementsByClassName('array')[0].innerHTML='Найдены совмещенные даты!';
-  }
-
-  //document.getElementsByClassName('array')[0].innerHTML='Найдены совмещенные даты!';
-   //document.getElementsByClassName('array')[0].classList.add("text-success");
-}
-
-});//конец функции
-});
-
-//  function удаление повторных записей(){
-    jQuery( function( $ ){ // есть разные варианты этой строчки, но такая мне нравится больше всего, т.к. всегда работает
-$( '#iddoubleclear' ).click( function(){ // при клике на элемент с id="idajax"
-//alert( 'JQuery у меня вроде работает!!!' ); // выводим сообщение просто для проверки работает ли jquery
-var ar=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
-var ll=ar.length;  //кол-во элементов с классом list-group-item
-let i;
-for(i=0;i<ll;i++){
-  if(ar[i].classList.contains("doubleremove")){  //проверка наличия класса в элементе
-ar[i].remove(); //удалить элемент
- }
-if(ar[i].classList.contains("bg-secondary")){  //удалить обозначение совмещенных дат
-  ar[i].classList.remove("bg-secondary");
-}
-
-
- document.getElementById('iddoubleclear').classList.remove("visible");
- document.getElementById('iddoubleclear').classList.add("invisible");
- document.getElementsByClassName('double')[0].innerHTML="";
-//document.getElementsByClassName('forwarning')[0].innerHTML="рекомендуется провести повторный перерасчет!";
-}
-document.getElementsByClassName('array')[0].innerHTML='';
-});
-});
-
-jQuery( function($){
-$( '#idajax1' ).click( function(){ // при клике на элемент с id="idajax"
-  var adress1="<?php
+jQuery( function($){ //рассчитать сумму разниц дат в таблице
+$( '#idajax1' ).click( function(){ // при клике на элемент с id="idajax1"
+  let adress1="<?php
   echo get_template_directory_uri().'/php/calculatedifdate1.php';//URL папки шаблона WordPress, например http://test/wp-content/themes/template
   ?>";
-
 let array1=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
-  var lengtharray1=array1.length;  //кол-во элементов с классом list-group-item
-  var array2=[];
+  let lengtharray1=array1.length;  //кол-во элементов с классом list-group-item
+  let array2=[];
 let i;
 for(i=0;i<lengtharray1;i++){
   //alert(array1[i].innerHTML);
   array2[i]=array1[i].innerHTML;
 }
-
 //ajax запрос
    $.ajax({
 	url: adress1, //'/index.php',         /* Куда пойдет запрос */
 	//method: 'get',             /* Метод передачи (post или get) */
 	dataType: 'html',          /* Тип данных в ответе (xml, json, script, html). */
 	//data:'param1='+123,     /* Параметры передаваемые в запросе. */
-
   type: 'POST',
   //data: {arr: arr},
   data: {array2: array2},
@@ -354,26 +216,124 @@ for(i=0;i<lengtharray1;i++){
 });
 });
 
+/////////////////////////////////////////////////
+
+    jQuery(function($){ // проверить список с датами
+$( '#idsort' ).click( function(){ // при клике на элемент с id="idsort"
+let ar=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
+let ll=ar.length;  //кол-во элементов с классом list-group-item
+
+let data1;
+let data2;
+let d1,d11;
+let d2,d22;
+let el,el0;
+let el1;
+let el2=1;
+let dd;
+let answer=0;//наличие совпадений
+let arr = []; //массив для всех дат
+
+//сортировка записей по возрастанию
+for(el0=0;el0<ll;el0++){  //0 цикл
+for(el1=0;el1<ll;el1++,el2++){  //1 цикл для первой даты
+  data1 = ar[el1].innerHTML.split('.', 3); //разбить строку на подстроки по знаку '.'
+  data2=data1[2].split('-',1);                  //разбить строку на подстроки по знаку '.-'
+  d1=data1[0]+'.'+data1[1]+'.'+data2[0];
+  d11=data2[0]+'.'+data1[1]+'.'+data1[0];
+for(el=el2;el<ll;el++){  //2 цикл для второй даты
+  data1 = ar[el].innerHTML.split('.', 3); //разбить строку на подстроки по знаку '.'
+  data2=data1[2].split('-',1);                  //разбить строку на подстроки по знаку '.-'
+  d2=data1[0]+'.'+data1[1]+'.'+data2[0];
+  d22=data2[0]+'.'+data1[1]+'.'+data1[0];
+  if(d11>d22){
+//ar[el1].after('after'); // вставить строку "after" ПОСЛЕ выбранного html элемента
+//ar[el1].before('before'); // вставить строку "before" ПЕРЕД выбранным html элементом
+ar[el1].before(ar[el]); // вставить строку "before" ПЕРЕД выбранным html элементом
+}
+}    //2 цикл
+}    //1 цикл
+}   //0 цикл
 
 
 
+//поиск одинаковых записей
+for(el=0;el<(ll-1);el++){
+  //поиск совпадений дат
+if(ar[el].innerHTML==ar[el+1].innerHTML){
+  answer=1;
+  // добавление элементу класс для удаления
+ar[el].classList.add("doubleremove");
+ar[el].classList.add("text-danger");
+}
+//поиск совмащенных дат
+}//конец цикла
+if(answer==1){
+document.getElementsByClassName('double')[0].innerHTML="Найдены совпадения!";
+document.getElementsByClassName('double')[0].classList.add("text-danger");
+document.getElementById('iddoubleclear').classList.remove("invisible");
+document.getElementById('iddoubleclear').classList.add("visible");
+}
+
+//поиск совмещенных дат
+let ar1=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
+let ll1=ar1.length;  //кол-во элементов с классом list-group-item
+let da1;
+let da2;
+let da11;
+let da21;
+let date1a;
+let date2a;
+let i;
+let answer1=0;//наличие совмещенных дат
+for(i=0;i<ll1;i++){
+  da1=ar1[i].innerHTML.split("-");
+  da2=ar1[i+1].innerHTML.split("-");
+let da11=da1[1].split(".");
+let da21=da2[0].split(".");
+  date1a = new Date(da11[2],da11[1],da11[0]);
+  date2a = new Date(da21[2],da21[1],da21[0]);
+  if(date1a>date2a){
+    ar1[i].classList.add("bg-warning");
+    ar1[i+1].classList.add("bg-warning");
+    answer1=1;
+  }
+if(answer1==1){
+  document.getElementsByClassName('array')[0].innerHTML='Найдены совмещенные даты!';
+  document.getElementsByClassName('array')[0].classList.add("bg-warning");
+  document.getElementById('iddoublejoin').classList.remove("invisible");
+  document.getElementById('iddoublejoin').classList.add("visible");
+}
+}
+});//конец функции
+});
+
+    jQuery( function( $ ){ //удалить повторяющиеся записи/даты
+$( '#iddoubleclear' ).click( function(){ // при клике на элемент с id="iddoubleclear"
+//alert( 'JQuery у меня вроде работает!!!' ); // выводим сообщение просто для проверки работает ли jquery
+let ar=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
+let ll=ar.length;  //кол-во элементов с классом list-group-item
+let i;
+for(i=0;i<ll;i++){
+  if(ar[i].classList.contains("doubleremove")){  //проверка наличия класса в элементе
+ar[i].remove(); //удалить элемент
+ }
+if(ar[i].classList.contains("bg-warning")){  //удалить обозначение совмещенных дат
+  ar[i].classList.remove("bg-warning");
+}
+ document.getElementById('iddoubleclear').classList.remove("visible");
+ document.getElementById('iddoubleclear').classList.add("invisible");
+ document.getElementById('iddoublejoin').classList.remove("visible");
+ document.getElementById('iddoublejoin').classList.add("invisible");
+ document.getElementsByClassName('double')[0].innerHTML="";
+ document.getElementsByClassName('array')[0].innerHTML='';
+}
+});
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-jQuery( function($){
+jQuery( function($){ //объединить совмещенные даты
 $( '#iddoublejoin' ).click( function(){ // при клике на элемент с id="iddoublejoin"
-
 
 let array1=document.getElementsByClassName('list-group-item');//поместить все list-group-item в массив
 let lengtharray1=array1.length;  //кол-во элементов с классом list-group-item
@@ -382,53 +342,37 @@ let i;
 let j=0;
 let data1;
 let data2;
+let answer2=0;
 for(i=0;i<lengtharray1;i++){
-  if(array1[i].classList.contains("bg-secondary")){
-
-
+  if(array1[i].classList.contains("bg-warning")){
     data1=array1[i].innerHTML.split('-'); //разбить строку на подстроки по знаку '.'
     data2=array1[i+1].innerHTML.split('-'); //разбить строку на подстроки по знаку '.'
-    alert(data1[0]+'-'+data2[1]);
-
+  //  alert(data1[0]+'-'+data2[1]);
      $(".list-group-item").eq(i).html(data1[0]+'-'+data2[1]); // содержание первого элемента меняется на data1[0]+" - "+data2[1]
-     array1[i].classList.remove("bg-secondary");
+     array1[i].classList.remove("bg-warning");
      $(".list-group-item").eq(i+1).remove();// удалить элемент с №1
   //  $(".list-group-item").eq(i+1).html(0); // содержание первого элемента меняется на data1[0]+" - "+data2[1]
 i++;
-//break;
+answer2=1;
+  }
+  if(answer2==1){
+    document.getElementById('iddoublejoin').classList.remove("visible");
+    document.getElementById('iddoublejoin').classList.add("invisible");
+    document.getElementsByClassName('array')[0].remove("bg-warning");
+    document.getElementsByClassName('array')[0].innerHTML='';
   }
 }
 
-
-
-
-
-
-
-
 });
 });
-
-///////////////////
           </script>
-          <?php
-          //расчет даты
-          /*$origin = new DateTimeImmutable('2009-10-11');
-          $target = new DateTimeImmutable('2009-10-13');
-          $interval = $origin->diff($target);
-          echo $interval->format('%R%a дней');
-          */
-                    ?>
 
-        <input type="button" class="btn btn-outline-info" value="добавить период между датами" id="idajax"/>
+        <input type="button" class="btn btn-outline-info" value="добавить даты" id="idajax"/>
+        <input type="button" class="btn btn-outline-info" value="проверить список на повторы" id="idsort"/>
         <input type="button" class="btn btn-outline-info" value="рассчитать сумму разниц дат в таблице" id="idajax1"/>
-
         <input type="button" class="btn btn-outline-info" value="сбросить" id="idclear"/>
-
-          <input type="button" class="btn btn-outline-info" value="проверить введеные даты" id="idsort"/>
-
-            <input type="button" class="btn btn-outline-info text-danger invisible" value="удалить повторяющиеся записи/даты" id="iddoubleclear"/>
-            <input type="button" class="btn btn-outline-info text-danger" value="объединить совмещенные даты" id="iddoublejoin"/>
+        <input type="button" class="btn btn-outline-info text-danger invisible" value="удалить повторяющиеся записи/даты" id="iddoubleclear"/>
+        <input type="button" class="btn btn-outline-info bg-warning invisible" value="объединить совмещенные даты" id="iddoublejoin"/>
 
             <!-- Конец своего кода -->
           <?php endwhile; ?>
